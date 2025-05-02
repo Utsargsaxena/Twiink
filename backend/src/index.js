18c414d4757d0803467d8f42a55fae7a64430b3a
Utsarg saxena <utsargsaxena@Utsargs-MacBook-Air.local>
2025-05-02 20:48:32 +0530

    fix: production static path & wildcard routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,12 @@ app.use("/api/messages" , MessageRoutes)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*" , (req,res) => {
-        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"));
-    })
+    app.get("/*", (req, res) => {
+        res.sendFile(
+          path.join(__dirname, "../frontend", "dist", "index.html")
+        );
+      });
+      
 }
 server.listen(PORT , () => {
     console.log("Server  is running on port :"+PORT);
